8cb41cb7f687a401b53f7adb3a50bf0bf9b57aa7
cryptolog <vetrovm398@gmail.com>
2019-06-21 14:53:36 +0700

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -169,9 +169,10 @@ Value getworkex(const Array& params, bool fHelp)
             "getworkex [data, coinbase]\n"
             "If [data, coinbase] is not specified, returns extended work data.\n"
         );
-
-    if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
+//51
+    
+  //  if (vNodes.empty())
+  //      throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "ARGUSCOIN is downloading blocks...");
@@ -309,9 +310,10 @@ Value getwork(const Array& params, bool fHelp)
             "  \"hash1\" : formatted hash buffer for second hash (DEPRECATED)\n" // deprecated
             "  \"target\" : little endian hash target\n"
             "If [data] is specified, tries to solve the block and returns true if it was successful.");
-
-    if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
+//51
+    
+    //if (vNodes.empty())
+    //    throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "ARGUSCOIN is downloading blocks...");
@@ -448,12 +450,13 @@ Value getblocktemplate(const Array& params, bool fHelp)
         else
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
     }
-
+    
     if (strMode != "template")
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
 
-    if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
+    //51
+    //if (vNodes.empty())
+    //    throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "ARGUSCOIN is not connected!");
 
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "ARGUSCOIN is downloading blocks...");
